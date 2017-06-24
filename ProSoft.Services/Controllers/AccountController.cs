25e31cd1b42cdaf728aa00cb3f56db08d1503c0f
Administrator <Administrator@ADMINISTRATORS>
2017-06-24 14:57:19 +0700

    Add API Account

---
+++ ProSoft.Services/Controllers/AccountController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ProSoft.Services.Controllers
+{
+    [Authorize]
+    [RoutePrefix("api/Account")]
+    public class AccountController : ApiController
+    {
+        [HttpPost]
+        [Route("Get")]
+        public IHttpActionResult Get(int id, object dto)
+        {
+            return Ok();
+        }
+    }
+}
